e2495f02dfa77082fc70785b34aac1d5c8197e32
djoshi2 <djoshi2@islander.tamucc.edu>
2025-05-21 14:52:35 -0500

    Added Files and Data

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="f1sight",
+    version="0.0.1",
+    author="Dhruti Joshi",
+    author_email="dhrutijoshi11@gmail.com",
+    packages=find_packages()
+)
